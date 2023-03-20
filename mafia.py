15511a36b0256c44cc99ac2e2aa4c64d519990ec
lindasayyeri <128362984+lindasayyeri@users.noreply.github.com>
2023-03-20 19:57:52 +0330

    update

--- mafia.py
+++ mafia.py
@@ -1,2 +1,90 @@
 #mafia game
-print('-----****** MAFIA ******-----')
+import random
+print("----****welcome to mafia game****----")
+finalroles=[]
+users=[]
+for i in range(9):
+    user=input("enter your name please : ")
+    users.append(user)
+print(users)
+roles=["doctor","karagah","sniper","shahrvand","shahrvand","zereh push","Boss","lover","sharlatan"]
+mafias=[]
+for item in roles:
+    finalroles=random.sample(roles,k=9)
+# print("----****Day 1****----")
+# user1=input(f"{users[0]} lotfan 30s sohbat konid : ")
+# print(f"{users[0]} : {user1}")
+# print("------------------------------------------------------------------------")
+# user2=input(f"{users[1]} lotfan 30s sohbat konid : ")
+# print(f"{users[1]} : {user2}")
+# print("------------------------------------------------------------------------")
+# user3=input(f"{users[2]} lotfan 30s sohbat konid : ")
+# print(f"{users[2]} : {user3}")
+# print("------------------------------------------------------------------------")
+# user4=input(f"{users[3]} lotfan 30s sohbat konid : ")
+# print(f"{users[3]} : {user4}")
+# print("------------------------------------------------------------------------")
+# user5=input(f"{users[4]} lotfan 30s sohbat konid : ")
+# print(f"{users[4]} : {user5}")
+# print("------------------------------------------------------------------------")
+# user6=input(f"{users[5]} lotfan 30s sohbat konid : ")
+# print(f"{users[5]} : {user6}")
+# print("------------------------------------------------------------------------")
+# user7=input(f"{users[6]} lotfan 30s sohbat konid : ")
+# print(f"{users[6]} : {user7}")
+# print("------------------------------------------------------------------------")
+# user8=input(f"{users[7]} lotfan 30s sohbat konid : ")
+# print(f"{users[7]} : {user8}")
+# print("------------------------------------------------------------------------")
+# user9=input(f"{users[8]} lotfan 30s sohbat konid : ")
+# print(f"{users[8]} : {user9}")
+# print("------------------------------------------------------------------------")
+# print("----****night 1****----")
+# print("mafia ha bidar shavand !!!")
+# if "lover" in test(dict.values()):
+#     print(test(dict["lover"]))
+# print("----****day 2****----")
+# user1=input(f"{users[0]} lotfan 30s sohbat konid : ")
+# print(f"{users[0]} : {user1}")
+# print("------------------------------------------------------------------------")
+# user2=input(f"{users[1]} lotfan 30s sohbat konid : ")
+# print(f"{users[1]} : {user2}")
+# print("------------------------------------------------------------------------")
+# user3=input(f"{users[2]} lotfan 30s sohbat konid : ")
+# print(f"{users[2]} : {user3}")
+# print("------------------------------------------------------------------------")
+# user4=input(f"{users[3]} lotfan 30s sohbat konid : ")
+# print(f"{users[3]} : {user4}")
+# print("------------------------------------------------------------------------")
+# user5=input(f"{users[4]} lotfan 30s sohbat konid : ")
+# print(f"{users[4]} : {user5}")
+# print("------------------------------------------------------------------------")
+# user6=input(f"{users[5]} lotfan 30s sohbat konid : ")
+# print(f"{users[5]} : {user6}")
+# print("------------------------------------------------------------------------")
+# user7=input(f"{users[6]} lotfan 30s sohbat konid : ")
+# print(f"{users[6]} : {user7}")
+# print("------------------------------------------------------------------------")
+# user8=input(f"{users[7]} lotfan 30s sohbat konid : ")
+# print(f"{users[7]} : {user8}")
+# print("------------------------------------------------------------------------")
+# user9=input(f"{users[8]} lotfan 30s sohbat konid : ")
+# print(f"{users[8]} : {user9}")
+# print("------------------------------------------------------------------------")
+# print("######  vote  ######")
+# vote = []
+# for i in range(9):
+#     user=input("enter your name please : ")
+#     vote.append(user)
+# votedict={}
+# i=0
+# while i<=9:
+#     votenum=vote.count(users[0])
+#     votedict{users[0]:votenum}
+#     i+=1
+
+    
+    
+
+
+
